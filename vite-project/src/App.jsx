eac547bd7025369cbc0b602a232c9dddd56b313d
MaximuxR93 <alocerkenoo7@hmail.com>
2025-01-16 13:59:18 +0530

    changed some files

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Weather from './components/Weather'
+
 const App = () => {
   return (
     <div className='app'>
--- vite-project/src/components/Weather.jsx
+++ vite-project/src/components/Weather.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
 import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
+import humidity_icon from '../assets/humidity.png'
+
+
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather</h1>
+    <div className='weather'>
+      <div className="search-bar">
+        <input type="text" placeholder='Search' />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt= "" className='weather-icon'/>
+      <p className='tempereture'>9c</p>
+      <p className='location'>London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>93%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>9.0 km/h</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Weather
